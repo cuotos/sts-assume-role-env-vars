0b6040157f6133e44e630136866bffcd2f668fb1
Dan Potepa <dan@danpotepa.co.uk>
2022-07-05 16:47:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+`aws sts assume-role` returns a json object containing the credentials in order to assume the requested role.
+
+pipe this output into this tool and it will provide you with the `export ENV_VAR` statements to set your shell to use the requested role.
+
+## Example
+
+```
+$ aws sts assume-role --role-arn arn:aws:iam::123456789123:role/requested-role --role-session-name a-session-name | sts-assume-role-env-vars
+export AWS_ACCESS_KEY_ID=ASIA....
+export AWS_SECRET_ACCESS_KEY=lakshjdjkahsd
+export AWS_SESSION_TOKEN=askljdakljsd
+```
+
+just copy and paste these and you'll be using the requested session / role.
+
---
+++ main_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseAssumeRoleOutput(t *testing.T) {
+	input := `{
+    "Credentials": {
+        "AccessKeyId": "ASIA...UUP2",
+        "SecretAccessKey": "fYS...in",
+        "SessionToken": "IQoJ...P46W4F/IEX",
+        "Expiration": "2022-07-05T14:28:30+00:00"
+    },
+    "AssumedRoleUser": {
+        "AssumedRoleId": "AROA...5YCM:danp",
+        "Arn": "arn:aws:sts::111111111111:assumed-role/runner-role/cuotos"
+    }
+}`
+
+	expected := ParsedAssumeRoleCreds{
+		AccessKeyID:  "ASIA...UUP2",
+		SecretKey:    "fYS...in",
+		SessionToken: "IQoJ...P46W4F/IEX",
+	}
+
+	actual, err := ParseAssumeRoleOutput(strings.NewReader(input))
+
+	require.NoError(t, err)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestOutputExportEnvVarsStatements(t *testing.T) {
+	input := ParsedAssumeRoleCreds{
+		AccessKeyID:  "accessKey",
+		SecretKey:    "secretKey",
+		SessionToken: "sessionToken",
+	}
+
+	expected := `export AWS_ACCESS_KEY_ID=accessKey
+export AWS_SECRET_ACCESS_KEY=secretKey
+export AWS_SESSION_TOKEN=sessionToken`
+
+	actual := GenerateSetEnvVarStatement(input)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestEndToEnd(t *testing.T) {
+	input := `{
+    "Credentials": {
+        "AccessKeyId": "accessKey",
+        "SecretAccessKey": "secretKey",
+        "SessionToken": "sessionToken",
+        "Expiration": "2022-07-05T14:28:30+00:00"
+    },
+    "AssumedRoleUser": {
+        "AssumedRoleId": "AROA...5YCM:danp",
+        "Arn": "arn:aws:sts::111111111111:assumed-role/runner-role/cuotos"
+    }
+}`
+
+	expected := `export AWS_ACCESS_KEY_ID=accessKey
+export AWS_SECRET_ACCESS_KEY=secretKey
+export AWS_SESSION_TOKEN=sessionToken`
+
+	actual, err := run(strings.NewReader(input))
+
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
